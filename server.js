f7847738804aa20e224fdcf359c5d083a217bc91
Connor Macqueen <connor.macqueen@and.digital>
2022-06-15 11:15:27 +0100

    Adding root path for html page

--- server.js
+++ server.js
@@ -1,10 +1,39 @@
 var express = require('express');
 var app = express();
 var fs = require("fs");
+const twilio = require('twilio');
+var path = require("path");
 
-app.get('/text-user', function (req, res) {
-   console.log(123);
-})
+require('dotenv').config();
+
+
+app.get("/", function (req, res){
+
+   res.sendFile(
+      path.join(__dirname, '/index.html')
+   );
+
+});
+
+app.post('/text-user', function (req, res) {
+   
+   // Find your account sid and auth token in your Twilio account Console.
+
+   const client = new twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+   const name = 'Connor';
+   const andTitle = 'Swimming Coach';
+   var message = `Hello Connor, its me, ${name}. My AND title is AND ${andTitle}! :)`;
+   var toNumber = '+447891187501';
+   // Send the text message.
+   client.messages.create({
+   to: toNumber,
+   from: process.env.MYTWILLO,
+   body: message
+   });
+
+   console.log("Text Sent!");
+});
+
 
 var server = app.listen(3000, function () {
    var host = server.address().address
