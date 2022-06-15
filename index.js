ecdbb3b9ad42ac72521cd37ff9da9bce8104c92d
Michael <michaeld55@hotmail.co.uk>
2022-06-15 09:55:02 +0100

    added phone num to env

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 var twilio = require('twilio');
 
 // Find your account sid and auth token in your Twilio account Console.
-
-//Require dotenv to hide details. 
 require('dotenv').config();
-//Getting tokens from the .env file
 var client = new twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 
 // Send the text message.
 client.messages.create({
-  to: '+447891187501',
-  from: '+19855453668',
+  to: '+447562872419',
+  from: process.env.MYTWILLO,
   body: 'Hello Connor, its me Parth. My AND title is AND Traveler! :)'
 });
