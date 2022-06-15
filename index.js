2564e24258c142099ed5b5f332230f7c01411940
Parth097 <parthsh197@gmail.com>
2022-06-15 09:41:34 +0100

    Adding connor's number to test

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 var twilio = require('twilio');
 
 // Find your account sid and auth token in your Twilio account Console.
+
+//Require dotenv to hide details. 
 require('dotenv').config();
+//Getting tokens from the .env file
 var client = new twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 
 // Send the text message.
 client.messages.create({
-  to: '+447562872419',
+  to: '+447891187501',
   from: '+19855453668',
   body: 'Hello Connor, its me Parth. My AND title is AND Traveler! :)'
 });
