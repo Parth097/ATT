d46b3e95bb58b8da41ea18bd0d04ada0a7f4dbd3
Connor Macqueen <connor.macqueen@and.digital>
2022-06-15 10:46:03 +0100

    change to mee

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const twilio = require('twilio');
 // Find your account sid and auth token in your Twilio account Console.
 require('dotenv').config();
 const client = new twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
-const name = 'Parth';
-const andTitle = 'Traveler';
+const name = 'Connor';
+const andTitle = 'Swimming Coach';
 var message = `Hello Connor, its me, ${name}. My AND title is AND ${andTitle}! :)`;
 var toNumber = '+447891187501';
 // Send the text message.
